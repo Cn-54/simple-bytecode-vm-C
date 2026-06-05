8e103ef10ffee669b4f5f7cdfc3fda8b407d402d
Cn-54 <harleywalker912@icloud.com>
2026-06-05 18:21:31 +0100

    added labels and add a countdown test program

--- src/assemble.c
+++ src/assemble.c
@@ -14,6 +14,14 @@ typedef struct {
     Opcode opcode;
 } OpcodeEntry;
 
+typedef struct {
+    char name[32];
+    int address;
+} Label;
+
+static Label labelTable[100];
+static int labelCount = 0;
+
 static const OpcodeEntry opcodeTable[] = {
     {"HALT", HALT},
     {"NOP",  NOP},
@@ -38,6 +46,19 @@ static const OpcodeEntry opcodeTable[] = {
     {"OUTI", OUTI},
     {"INPI", INPI}
 };
+
+
+static int isLabel(const char *s) {
+    size_t n = strlen(s);
+    return n > 0 && s[n - 1] == ':';
+}
+
+static void stripColon(char *s) {
+    size_t n = strlen(s);
+    if (s[n - 1] == ':')
+        s[n - 1] = '\0';
+}
+
 
 static Tokens tokenise(char line[]){
     Tokens t = {0};
@@ -69,6 +90,22 @@ static Opcode getOpcode(const char *str){
     return HALT;
 }
 
+static int findLabel(const char *name) {
+    for (int i = 0; i < labelCount; i++) {
+        if (strcmp(labelTable[i].name, name) == 0)
+            return labelTable[i].address;
+    }
+    return -1;
+}
+
+int resolveOperand(const char *s) {
+    int addr = findLabel(s);
+    if (addr == -1) {
+        printf("Undefined label: %s\n", s);
+        return 0;
+    }
+    return addr;
+}
 
 int assemble(const char *input, const char *output){
     FILE *in = fopen(input, "r");
@@ -81,6 +118,29 @@ int assemble(const char *input, const char *output){
     }
 
     char line[256];
+    int pc = 0;
+
+    while (fgets(line, sizeof(line), in)) {
+        line[strcspn(line, "\r\n")] = 0;
+        Tokens t = tokenise(line);
+
+        if (t.count == 0) continue;
+
+        if (isLabel(t.words[0])) {
+            stripColon(t.words[0]);
+
+            strcpy(labelTable[labelCount].name, t.words[0]);
+            labelTable[labelCount].address = pc;
+            labelCount++;
+
+            continue;
+        }
+
+        pc++;
+    }
+
+    rewind(in);
+    pc = 0;
 
     while (fgets(line, sizeof(line), in))
     {
@@ -91,6 +151,8 @@ int assemble(const char *input, const char *output){
         if (t.count == 0)
             continue;
 
+        if (isLabel(t.words[0])) continue;
+
         Opcode op = getOpcode(t.words[0]);
 
         int op1 = 0, op2 = 0, op3 = 0;
@@ -99,6 +161,7 @@ int assemble(const char *input, const char *output){
         if (t.count > 2) op2 = atoi(t.words[2]);
         if (t.count > 3) op3 = atoi(t.words[3]);
 
+        int addr;
         switch (op)
         {
             case HALT:
@@ -122,12 +185,14 @@ int assemble(const char *input, const char *output){
                 Emit(out, op, op1, op2, op3);
                 break;
             case JMP:
-                Emit(out, op, op1, 0,0);
+                addr = resolveOperand(t.words[1]);
+                Emit(out, op, 0, 0, addr);
                 break;
             case JGT:
             case JLT:
             case JEQ:
-                Emit(out, op, op1, op2, op3);
+                addr = resolveOperand(t.words[3]);
+                Emit(out, op, op1, op2, addr);
                 break;
             case OUT:
             case PUT:
